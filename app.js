f44bd144bddaea169c11ce0e7a1b09cc31f232e4
rata854 <nitiham2006@gmail.com>
2021-05-13 16:14:59 +0900

    [Finish]js_2

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
-alert('Hello World');
+// var orange = 1000;
+// var apple = 20000;
+
+// if(orange < apple){
+//   alert('みかんの値段がりんごより安い');
+// } else if(orange == apple){
+//   alert('みかんとりんごが同じ値段');
+// } else{
+//   alert('みかんの値段がリンゴより高い');
+// }
+
+// var max = 200;
+// var num = 1;
+// var count = 0;
+
+// while(num < max){
+//   num = num * 2;
+//   count = count + 1;
+// }
+
+// alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です。');
+
+// var i;
+// var num = 0;
+
+// for(i=1; i<11; i++){
+//   num = num + i;
+// }
+
+// alert('1から10まで足し算した結果は' + num + 'です。');
+
+// 確認問題
+
+alert(30 + 66);
+
+alert(55 - 22);
+
+alert(63 * 99);
+
+alert(22 / 11);
